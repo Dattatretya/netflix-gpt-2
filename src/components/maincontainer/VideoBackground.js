5e3f415265a9e02931f590cd273253d1281e95df
Dattatretya <dattatreyasengupta.work@gmail.com>
2024-03-28 21:09:10 +0530

    browse page2

--- src/components/maincontainer/VideoBackground.js
+++ src/components/maincontainer/VideoBackground.js
@@ -9,9 +9,9 @@ const VideoBackground = ({movie_id}) => {
   useMovieTrailer(movie_id)
 
   return (
-    <div> 
-      <iframe width="560" height="315" 
-      src={"https://www.youtube.com/embed/"+trailerId+"?si=YkZaNQlzFvQU6H6j"}
+    <div className=''> 
+      <iframe className='w-screen aspect-video'
+      src={"https://www.youtube.com/embed/"+trailerId+"?&autoplay=1&mute=1"}
       title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" ></iframe>
     </div>
   )
--- src/components/maincontainer/VideoTitle.js
+++ src/components/maincontainer/VideoTitle.js
@@ -3,12 +3,12 @@ import React from 'react'
 const VideoTitle = ({title, overview}) => {
 
   return (
-    <div className='pt-28 px-12 m-4'>
+    <div className='w-screen aspect-video pt-28 px-12 absolute text-white bg-gradient-to-r from-black'>
         <h1 className='text-4xl font-bold'>{title}</h1>
-        <p className='py-6 text-lg w-1/3'>{overview}</p>
+        <p className='py-6 text-lg w-3/4'>{overview}</p>
         <div className=''>
-            <button className='m-1 p-1 text-lg bg-white border border-black rounded-md w-24 opacity'>▶ Play</button>
-            <button className='m-1 p-1 px-2 text-lg bg-gray-400 opacity-80 text-white w-32 rounded-md'>ℹ More Info</button>
+            <button className='m-1 p-1 text-lg text-black bg-white border border-black rounded-md w-24 hover:bg-opacity-85'>▶ Play</button>
+            <button className='m-1 p-1 px-2 text-lg bg-gray-600 opacity-90 hover:bg-opacity-85 text-white w-32 rounded-md'>More Info</button>
         </div>
     </div>
   )
