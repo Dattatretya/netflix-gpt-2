5eb48f126f00269a1a5cf4ebfe29074643f737d1
Dattatretya <dattatreyasengupta.work@gmail.com>
2024-04-24 20:21:29 +0530

    gpt

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,14 @@ export const AVATAR = 'https://occ-0-3752-3647.1.nflxso.net/dnm/api/v6/vN7bi_My8
 
 export const BACKGROUND = "https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/a3873901-5b7c-46eb-b9fa-12fea5197bd3/IN-en-20240311-popsignuptwoweeks-perspective_alpha_website_small.jpg"
 
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer ' + process.env.REACT_APP_TMDB_KEY
+    }
+  };
+
+export const IMG_CDN = "https://image.tmdb.org/t/p/w500/"
+
+export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY
