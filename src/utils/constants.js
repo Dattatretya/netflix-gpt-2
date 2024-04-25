5911ba798e65964bc682bdb1314eaa92815e80e2
Dattatretya <dattatreyasengupta.work@gmail.com>
2024-04-25 17:18:28 +0530

    gpt

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
-export const LOGO = process.env.REACT_APP_LOGO
+export const LOGO = 'https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
 
-export const AVATAR = process.env.REACT_APP_AVATAR
+export const AVATAR = 'https://occ-0-3752-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABQZLu3tbiSuiiDBp-icDwSRXAVSxEcUzCTRESSgt1RM8vzuAPshbi2d43p6m53ljhIGT4LEPJU4smKHqIN89iHxMOKFWJOc.png?r=93c'
 
-export const BACKGROUND = process.env.REACT_APP_BACKGROUND
+export const BACKGROUND = "https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/a3873901-5b7c-46eb-b9fa-12fea5197bd3/IN-en-20240311-popsignuptwoweeks-perspective_alpha_website_small.jpg"
 
 export const API_OPTIONS = {
     method: 'GET',
